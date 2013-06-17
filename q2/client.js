8500ba03694e0ba7c1d640d172ed12d788dc2880
William Humphreys-Cloutier <williamhumphreyscloutier@gmail.com>
2013-06-17 09:03:03 -0500

    fixed js for users

--- q2/client.js
+++ q2/client.js
@@ -3,18 +3,19 @@ function replace_user_list (users) {
   select.innerHTML = ""
   for (var i = users.length - 1; i >= 0; i--) {
     user = users[i]
-    select.innerHTML += "<option" + user.selected ? " selected=true": "" +
-     ">" + user.name + "</option>"
+    select.innerHTML += "<option" + (user.selected ? " selected=true": "") +
+     ">" + user.username + "</option>"
   }
 }
 
+function handle_user_response(){
+  users = JSON.parse(this.responseText)
+  replace_user_list(users)
+}
+
 function get_users(users) {
-  user_request = new XMLHttpRequest({
-    onload: function(){
-      users = JSON.parse(this.responseText)
-      replace_user_list(users)
-    }
-  })
+  user_request = new XMLHttpRequest()
+  user_request.onload = handle_user_response 
   user_request.open('GET', 'users.cgi')
   user_request.send()
 }
